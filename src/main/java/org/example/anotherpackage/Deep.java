e04a3019159122177e356dd8d7d5cb175ebec1df
BelhardTraining <an.maevskaya@gmail.com>
2023-11-16 20:29:57 +0300

    Initial framework commit

---
+++ src/main/java/org/example/anotherpackage/Deep.java
@@ -0,0 +1,4 @@
+package org.example.anotherpackage;
+
+public class Deep {
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public static void firstTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://litecart.stqa.ru/en/rubber-ducks-c-1/red-duck-p-3");
+    }
+}
