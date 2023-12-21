6b46b283321bb14c90b2d0f0fb8288bfcd19ae11
BelhardTraining <an.maevskaya@gmail.com>
2023-12-21 17:33:09 +0300

    Updated POM to support execution of testng.xml from command line Added Step annotation to the new step

--- src/test/java/patterns/staticpo/NewCustomerPage.java
+++ src/test/java/patterns/staticpo/NewCustomerPage.java
@@ -1,13 +1,16 @@
 package patterns.staticpo;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 import static patterns.staticpo.Locators.getLocator;
 
 public class NewCustomerPage {
 
+    @Step("Validating new customer page title")
     public static void validatePageTitle() throws Exception {
+
         $(getLocator("newCustomerPage.createAccountTitle")).shouldHave(Condition.text("Create Account"));
     }
 }
